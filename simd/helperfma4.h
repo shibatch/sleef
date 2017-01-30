c01821a12268a5f5021ec8e050672582fba3025a
lithium\n-sibata <shibatch@users.sourceforge.net>
2017-01-30 17:18:13 +0900

    Renamed LICENSE_1_0.txt to LICENSE.txt.

--- simd/helperfma4.h
+++ simd/helperfma4.h
@@ -1,6 +1,6 @@
 //          Copyright Naoki Shibata 2010 - 2017.
 // Distributed under the Boost Software License, Version 1.0.
-//    (See accompanying file LICENSE_1_0.txt or copy at
+//    (See accompanying file LICENSE.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef __FMA4__
