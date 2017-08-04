a4d6be413085f9f2eb236b8d3e2109e7383b3650
Diana Bite <diana.bite@arm.com>
2017-08-04 14:06:20 +0100

    Code cleanup and minor reformat
    * Rebase include paths and document the constraints that apply
    * Privatise compiler options and compiler definitions
    * Slight change to how we call the sleef header generating script

--- cmake/Scripts/GenerateRenameHeader.cmake
+++ cmake/Scripts/GenerateRenameHeader.cmake
@@ -2,19 +2,18 @@ include(${LOCATIONS_FILE})
 
 string(REPLACE " " ";" RENAME_HEADER_LIST  "${RENAME_HEADERS}")
 
-set(sse2_params 2 4 sse2)
-
 foreach(rename_header ${RENAME_HEADER_LIST})
-  if(${rename_header} MATCHES "sse")
-    execute_process(
-	COMMAND ./mkrename ${sse2_params}
-	WORKING_DIRECTORY ${LOCATION_RUNTIME_DIR}
-	OUTPUT_VARIABLE MKRENAME_OUTPUT)
-    file(WRITE ${rename_header} "${MKRENAME_OUTPUT}")
-
-    if(OPTION_SHOW_CONFIG)
-      message(STATUS "Generating ${rename_header}")
-    endif(OPTION_SHOW_CONFIG)
+  if(${rename_header} MATCHES "renamesse2")
+    set(params 2 4 sse2)
   endif()
+
+  execute_process(
+    COMMAND ${LOCATION_RUNTIME_DIR}/${TARGET_MKRENAME} ${params}
+    OUTPUT_VARIABLE MKRENAME_OUTPUT)
+  file(WRITE ${rename_header} "${MKRENAME_OUTPUT}")
+
+  if(OPTION_SHOW_CONFIG)
+    message(STATUS "Generating ${rename_header}")
+  endif(OPTION_SHOW_CONFIG)
 endforeach()
 
