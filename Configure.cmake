13c3d0a39f5dc6331fbba055c2c945bfebba3b9e
Diana Bite <diana.bite@arm.com>
2017-08-14 13:28:27 +0100

    [Experimental] Add support for the remaining x86 vectors: FMA4, AVX2, AVX2128, AVX512F

--- Configure.cmake
+++ Configure.cmake
@@ -8,6 +8,8 @@ if(CMAKE_SYSTEM_PROCESSOR MATCHES "x86")
   set(SLEEF_ARCH_X86 ON CACHE INTERNAL "True for x86 architecture.")
 elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64")
   set(SLEEF_ARCH_AARCH64 ON CACHE INTERNAL "True for Aarch64 architecture.")
+  # Aarch64 requires support for advsimdfma4
+  set(COMPILER_SUPPORTS_ADVSIMD 1)
 endif()
 
 # COMPILER DETECTION
@@ -23,6 +25,8 @@ if(CMAKE_C_COMPILER_ID MATCHES "(GNU|Clang)")
   set(FLAGS_ENABLE_SSE2 "-msse2")
   set(FLAGS_ENABLE_SSE4 "-msse4.1")
   set(FLAGS_ENABLE_AVX "-mavx")
+  set(FLAGS_ENABLE_FMA4 "-mfma4")
+  set(FLAGS_ENABLE_AVX2 "-mavx2;-mfma")
 
   set(FLAGS_ENABLE_ADVSIMD "-march=armv8-a+simd")
 endif()
@@ -71,15 +75,38 @@ CHECK_C_SOURCE_COMPILES("
     __m256d r = _mm256_add_pd(_mm256_set1_pd(1), _mm256_set1_pd(2));
   }" COMPILER_SUPPORTS_AVX)
 
-set (CMAKE_REQUIRED_FLAGS ${FLAGS_ENABLE_ADVSIMD})
+set (CMAKE_REQUIRED_FLAGS ${FLAGS_ENABLE_FMA4})
 CHECK_C_SOURCE_COMPILES("
-  #include <arm_neon.h>
-  int main() {
-    double x[2];
-    float64x2_t vx = vld1q_f64(x);
-    vx = vaddq_f64(vx, vx);
-    vst1q_f64(x, vx);
-    }" COMPILER_SUPPORTS_ADVSIMD)
+#if defined(_MSC_VER)
+#include <intrin.h>
+#else
+#include <x86intrin.h>
+#endif
+int main() {
+  __m256d r = _mm256_macc_pd(_mm256_set1_pd(1), _mm256_set1_pd(2), _mm256_set1_pd(3));
+}" COMPILER_SUPPORTS_FMA4)
+
+set (CMAKE_REQUIRED_FLAGS ${FLAGS_ENABLE_AVX2})
+CHECK_C_SOURCE_COMPILES("
+#if defined(_MSC_VER)
+#include <intrin.h>
+#else
+#include <x86intrin.h>
+#endif
+int main() {
+  __m256i r = _mm256_abs_epi32(_mm256_set1_epi32(1));
+}" COMPILER_SUPPORTS_AVX2)
+
+set (CMAKE_REQUIRED_FLAGS ${FLAGS_ENABLE_AVX512F})
+CHECK_C_SOURCE_COMPILES("
+#if defined(_MSC_VER)
+#include <intrin.h>
+#else
+#include <x86intrin.h>
+#endif
+int main() {
+  __m512d r = _mm512_add_pd(_mm512_set1_pd(1), _mm512_set1_pd(2));
+}" COMPILER_SUPPORTS_AVX512F)
 
 # Reset used flags
 set(CMAKE_REQUIRED_FLAGS)
