d6cb3e84fb20fa7485751c4bf9b1409d1a7c60b6
Diana Bite <diana.bite@arm.com>
2017-08-21 13:13:40 +0100

    [Experimental] Enable cross-compilation for aarch64.
    * Adds dependencies as extra libraries to be linked against
    * Assumes a toolchain file which sets the compiler and target environment
    * Also assumes QEMU is installed on the host system
    * TODO: Detect if cross-compiling and run tests through interpreter
    * TODO: Build code generators natively and use as imported executables

---
+++ cmake/Toolchains/TC-aarch64-linux-gnu.cmake
@@ -0,0 +1,12 @@
+et(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR "aarch64")
+
+set(CMAKE_FIND_ROOT_PATH "/usr/aarch64-linux-gnu/")
+
+# Compilers to use
+set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER aarch64-linux-gnu-gcc++)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
