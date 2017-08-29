af63ee4c9ddb3339ebcb66b49064bcef65ac960f
Diana Bite <diana.bite@arm.com>
2017-08-29 19:14:32 +0100

    Use pushd and popd to preserve the state of the current directory.

--- cmake/Toolchains/TC-aarch64-linux-gnu.cmake
+++ cmake/Toolchains/TC-aarch64-linux-gnu.cmake
@@ -1,7 +1,8 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR "aarch64")
 
-set(CMAKE_FIND_ROOT_PATH "/usr/aarch64-linux-gnu/")
+set(CMAKE_FIND_ROOT_PATH "/usr/aarch64-linux-gnu/"i
+  "/home/travis/xcompile-extra-install")
 
 # Compilers to use
 set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
