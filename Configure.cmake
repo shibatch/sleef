b6d7aef6de437295cb9fffdded11eacc547aea3a
Alexandre Mutel <alexandre_mutel@live.com>
2017-09-25 00:01:37 +0200

    Various fixes to compile SLEEF on Windows with MSVC. Tested also on Ubuntu 14,04. Simplify the cmake setup (not using any cmake -E)

--- Configure.cmake
+++ Configure.cmake
@@ -2,6 +2,9 @@ include(CheckCCompilerFlag)
 include(CheckCSourceCompiles)
 include(CheckTypeSize)
 
+# Check that the library LIB_MPFR is available
+find_library(LIB_MPFR mpfr)
+
 # The library currently supports the following SIMD architectures
 set(SLEEF_SUPPORTED_EXTENSIONS
   SSE2 SSE4 AVX FMA4 AVX2 AVX2128 AVX512F # x86
@@ -19,43 +22,114 @@ if(NOT CMAKE_BUILD_TYPE)
     "Debug" "Release" "RelWithDebInfo" "MinSizeRel")
 endif()
 
-# PLATFORM DETECTION
+# Function used to generate safe command arguments for add_custom_command
+function(command_arguments PROPNAME)
+  set(quoted_args "")
+  foreach(arg ${ARGN})
+    list(APPEND quoted_args "\"${arg}\"" )
+  endforeach()
+  set(${PROPNAME} ${quoted_args} PARENT_SCOPE)
+endfunction()
 
-if(CMAKE_SYSTEM_PROCESSOR MATCHES "x86")
+
+# PLATFORM DETECTION
+if((CMAKE_SYSTEM_PROCESSOR MATCHES "x86") OR (CMAKE_SYSTEM_PROCESSOR MATCHES "AMD64"))
   set(SLEEF_ARCH_X86 ON CACHE INTERNAL "True for x86 architecture.")
+
+  set(SLEEF_HEADER_LIST
+    SSE_
+    SSE2
+    SSE4
+    AVX_
+    AVX
+    FMA4
+    AVX2
+    AVX2128
+    AVX512f
+  )
+  command_arguments(HEADER_PARAMS_SSE_      2 4 __m128d __m128 __m128i __m128i __SSE2__)
+  command_arguments(HEADER_PARAMS_SSE2      2 4 __m128d __m128 __m128i __m128i __SSE2__ sse2)
+  command_arguments(HEADER_PARAMS_SSE4      2 4 __m128d __m128 __m128i __m128i __SSE2__ sse4)
+  command_arguments(HEADER_PARAMS_AVX_      4 8 __m256d __m256 __m128i "struct { __m128i x, y; }" __AVX__)
+  command_arguments(HEADER_PARAMS_AVX       4 8 __m256d __m256 __m128i "struct { __m128i x, y; }" __AVX__ avx)
+  command_arguments(HEADER_PARAMS_FMA4      4 8 __m256d __m256 __m128i "struct { __m128i x, y; }" __AVX__ fma4)
+  command_arguments(HEADER_PARAMS_AVX2      4 8 __m256d __m256 __m128i __m256i __AVX__ avx2)
+  command_arguments(HEADER_PARAMS_AVX2128   2 4 __m128d __m128 __m128i __m128i __SSE2__ avx2128)
+  command_arguments(HEADER_PARAMS_AVX512f   8 16 __m512d __m512 __m256i __m512i __AVX512F__ avx512f)
+
 elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "aarch64")
   set(SLEEF_ARCH_AARCH64 ON CACHE INTERNAL "True for Aarch64 architecture.")
   # Aarch64 requires support for advsimdfma4
   set(COMPILER_SUPPORTS_ADVSIMD 1)
+
+  set(SLEEF_HEADER_LIST
+    ADVSIMD
+  )
+  set(HEADER_PARAMS_ADVSIMD    "2 4 float64x2_t float32x4_t int32x2_t int32x4_t __ARM_NEON advsimd")
+
 endif()
 
 # Enable building of the GNU ABI version for x86 and aarch64
+if(NOT MSVC)
+  if((SLEEF_ARCH_X86 OR SLEEF_ARCH_AARCH64))
+    set(SLEEF_ENABLE_GNUABI ON CACHE INTERNAL "Build GNU ABI compatible version.")
+  endif()
+endif()  
 
-if(SLEEF_ARCH_X86 OR SLEEF_ARCH_AARCH64)
-  set(SLEEF_ENABLE_GNUABI ON CACHE INTERNAL "Build GNU ABI compatible version.")
-endif()
+# MKRename arguments per type
+command_arguments(RENAME_PARAMS_SSE2           2 4 sse2)
+command_arguments(RENAME_PARAMS_SSE4           2 4 sse4)
+command_arguments(RENAME_PARAMS_AVX            4 8 avx)
+command_arguments(RENAME_PARAMS_FMA4           4 8 fma4)
+command_arguments(RENAME_PARAMS_AVX2           4 8 avx2)
+command_arguments(RENAME_PARAMS_AVX2128        2 4 avx2128)
+command_arguments(RENAME_PARAMS_AVX512f        8 16 avx512f)
+command_arguments(RENAME_PARAMS_ADVSIMD        2 4 advsimd)
+
+command_arguments(RENAME_PARAMS_GNUABI_SSE2    sse2 b 2 4 _mm128d _mm128 _mm128i _mm128i __SSE2__)
+command_arguments(RENAME_PARAMS_GNUABI_AVX     avx c 4 8 __m256d __m256 __m128i "struct { __m128i x, y; }" __AVX__)
+command_arguments(RENAME_PARAMS_GNUABI_AVX2    avx2 d 4 8 __m256d __m256 __m128i __m256i __AVX2__)
+command_arguments(RENAME_PARAMS_GNUABI_AVX512f avx512f e 8 16 __m512d __m512 __m256i __m512i __AVX512F__)
+command_arguments(RENAME_PARAMS_GNUABI_ADVSIMD advsimd n 2 4 float64x2_t float32x4_t int32x2_t int32x4_t __ARM_NEON)
 
 # COMPILER DETECTION
+
+# Allow to define the Gcc/Clang here
+# As we might compile the lib with MSVC, but generates bitcode with CLANG
+# Intel vector extensions.
+set(CLANG_FLAGS_ENABLE_SSE2 "-msse2")
+set(CLANG_FLAGS_ENABLE_SSE4 "-msse4.1")
+set(CLANG_FLAGS_ENABLE_AVX "-mavx")
+set(CLANG_FLAGS_ENABLE_FMA4 "-mfma4")
+set(CLANG_FLAGS_ENABLE_AVX2 "-mavx2;-mfma")
+set(CLANG_FLAGS_ENABLE_AVX2128 "-mavx2;-mfma")
+set(CLANG_FLAGS_ENABLE_AVX512F "-mavx512f")
+set(CLANG_FLAGS_ENABLE_NEON "")
+# Arm AArch64 vector extensions.
+set(CLANG_FLAGS_ENABLE_ADVSIMD "-march=armv8-a+simd")
 
 # All variables storing compiler flags should be prefixed with FLAGS_
 if(CMAKE_C_COMPILER_ID MATCHES "(GNU|Clang)")
   # Always compile sleef with -ffp-contract.
   set(FLAGS_STRICTMATH "-ffp-contract=off")
 
+  foreach(SIMD ${SLEEF_SUPPORTED_EXTENSIONS})
+    set(FLAGS_ENABLE_${SIMD} ${CLANG_FLAGS_ENABLE_${SIMD}})
+  endforeach()
+
   # Warning flags.
   set(FLAGS_WALL "-Wall -Wno-unused -Wno-attributes")
-
-  # Intel vector extensions.
-  set(FLAGS_ENABLE_SSE2 "-msse2")
-  set(FLAGS_ENABLE_SSE4 "-msse4.1")
-  set(FLAGS_ENABLE_AVX "-mavx")
-  set(FLAGS_ENABLE_FMA4 "-mfma4")
-  set(FLAGS_ENABLE_AVX2 "-mavx2;-mfma")
-  set(FLAGS_ENABLE_AVX2128 "-mavx2;-mfma")
-  set(FLAGS_ENABLE_AVX512F "-mavx512f")
-
+elseif(MSVC)
+  set(FLAGS_ENABLE_SSE2 /D__SSE2__)
+  set(FLAGS_ENABLE_SSE4 /D__SSE2__ /D__SSE3__ /D__SSE4_1__)
+  set(FLAGS_ENABLE_AVX  /D__SSE2__ /D__SSE3__ /D__SSE4_1__ /D__AVX__ /arch:AVX)
+  set(FLAGS_ENABLE_FMA4 /D__SSE2__ /D__SSE3__ /D__SSE4_1__ /D__AVX__ /D__AVX2__ /D__FMA4__ /arch:AVX2)
+  set(FLAGS_ENABLE_AVX2 /D__SSE2__ /D__SSE3__ /D__SSE4_1__ /D__AVX__ /D__AVX2__ /arch:AVX2)
+  set(FLAGS_ENABLE_AVX2128 /D__SSE2__ /D__SSE3__ /D__SSE4_1__ /D__AVX__ /D__AVX2__ /arch:AVX2)
+  set(FLAGS_ENABLE_AVX2512F /D__SSE2__ /D__SSE3__ /D__SSE4_1__ /D__AVX__ /D__AVX2__ /arch:AVX2)
+  set(FLAGS_ENABLE_NEON "")
   # Arm AArch64 vector extensions.
-  set(FLAGS_ENABLE_ADVSIMD "-march=armv8-a+simd")
+  set(FLAGS_ENABLE_ADVSIMD "")
 endif()
 
 set(SLEEF_C_FLAGS "${FLAGS_WALL} ${FLAGS_STRICTMATH}")
