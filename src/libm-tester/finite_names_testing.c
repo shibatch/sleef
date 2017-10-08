cabea6d12af3e6922157eeb7f491fab26900d01c
Francesco Petrogalli <francesco.petrogalli@arm.com>
2017-10-08 20:20:49 +0100

    [cmake] Add finite name testing for SSE2.

---
+++ src/libm-tester/finite_names_testing.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#ifdef __SSE2__
+#define CONFIG 2
+#include "helpersse2.h"
+#define vcos_finite _ZGbN2v___cos_finite
+#define vsin_finite _ZGbN2v___sin_finite
+// #endif
+
+extern vdouble vcos_finite(vdouble);
+extern vdouble vsin_finite(vdouble);
+
+int main(void) {
+  double in[VECTLENDP], out[VECTLENDP];
+  vdouble vin, vout;
+  vin = vload_vd_p(in);
+  vout = vcos_finite(vin);
+  vout = vsin_finite(vout);
+  vstore_v_p_vd(out, vout);
+
+  for (int i = 0; i < VECTLENDP; ++i)
+    printf("%g\n", out[i]);
+
+  return 0;
+}
+
+#else
+
+int main(void) { printf("Nothing here.\n"); }
+
+#endif
