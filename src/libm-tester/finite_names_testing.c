13a25f10b8a8476e20f2559133ad4a43dcdd69fb
Francesco Petrogalli <francesco.petrogalli@arm.com>
2017-10-08 20:31:10 +0000

    [cmake] Fix compilation.

--- src/libm-tester/finite_names_testing.c
+++ src/libm-tester/finite_names_testing.c
@@ -1,26 +1,45 @@
 #include <stdio.h>
 
-#ifdef ENABLE_SSE2
-#define CONFIG 2
-#include "helpersse2.h"
-#define vcos_finite _ZGVbN2v___cos_finite
-#define vsin_finite _ZGVbN2v___sin_finite
-// #endif
+#if defined(ENABLE_SSE2) || defined(ENABLE_SSE4)
 
-extern vdouble vcos_finite(vdouble);
-extern vdouble vsin_finite(vdouble);
+#ifdef ENABLE_SSE2
+#define NAME "SSE2"
+#define CONFIG 2
+#endif
+
+#ifdef ENABLE_SSE4
+#define NAME "SSE4"
+#define CONFIG 4
+#endif
+
+#include "helpersse2.h"
+#define vacos_finite _ZGVbN2v___acos_finite
+#define vasin_finite _ZGVbN2v___asin_finite
+
+#endif /* defined(ENABLE_SSE2) || defined(ENABLE_SSE4) */
+
+#ifdef NAME
+
+extern vdouble vacos_finite(vdouble);
+extern vdouble vasin_finite(vdouble);
 
 int main(void) {
   double in[VECTLENDP], out[VECTLENDP];
   vdouble vin, vout;
   int i;
-  vin = vload_vd_p(in);
-  vout = vcos_finite(vin);
-  vout = vsin_finite(vout);
-  vstore_v_p_vd(out, vout);
 
   for (i = 0; i < VECTLENDP; ++i)
-    printf("%g\n", out[i]);
+    in[i] = i;
+
+  vin = vload_vd_p(in);
+  vout = vacos_finite(vin);
+  vout = vasin_finite(vout);
+  vstore_v_p_vd(out, vout);
+
+  printf("%s = ", NAME);
+  for (i = 0; i < VECTLENDP; ++i)
+    printf("%g ", out[i]);
+  printf("\n");
 
   return 0;
 }
