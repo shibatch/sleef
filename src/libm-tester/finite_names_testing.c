d2b59d307f1ed4167dab2711934aea1f6624ca69
Francesco Petrogalli <francesco.petrogalli@arm.com>
2017-10-08 20:50:43 +0000

    [cmake] Add finite tests for AVX, AVX2 and AVX512F.

--- src/libm-tester/finite_names_testing.c
+++ src/libm-tester/finite_names_testing.c
@@ -18,6 +18,30 @@
 
 #endif /* defined(ENABLE_SSE2) || defined(ENABLE_SSE4) */
 
+#ifdef ENABLE_AVX
+#define NAME "AVX"
+#define CONFIG 1
+#include "helperavx.h"
+#define vacos_finite _ZGVcN4v___acos_finite
+#define vasin_finite _ZGVcN4v___asin_finite
+#endif
+
+#ifdef ENABLE_AVX2
+#define NAME "AVX2"
+#define CONFIG 1
+#include "helperavx2.h"
+#define vacos_finite _ZGVdN4v___acos_finite
+#define vasin_finite _ZGVdN4v___asin_finite
+#endif
+
+#ifdef ENABLE_AVX512F
+#define NAME "AVX512F"
+#define CONFIG 1
+#include "helperavx512f.h"
+#define vacos_finite _ZGVeN8v___acos_finite
+#define vasin_finite _ZGVeN8v___asin_finite
+#endif
+
 #ifdef NAME
 
 extern vdouble vacos_finite(vdouble);
@@ -36,10 +60,10 @@ int main(void) {
   vout = vasin_finite(vout);
   vstore_v_p_vd(out, vout);
 
-  printf("%s = ", NAME);
+  printf("%s... ", NAME);
   for (i = 0; i < VECTLENDP; ++i)
     printf("%g ", out[i]);
-  printf("\n");
+  printf(" ...passed. \n");
 
   return 0;
 }
