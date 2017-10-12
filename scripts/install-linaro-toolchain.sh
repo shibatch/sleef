f56ab29cb2099dde5864592e0dd0d100bf639459
Francesco Petrogalli <francesco.petrogalli@arm.com>
2017-10-12 21:18:51 +0100

    fix script

--- scripts/install-linaro-toolchain.sh
+++ scripts/install-linaro-toolchain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 TXZ=https://releases.linaro.org/components/toolchain/binaries/latest/aarch64-linux-gnu/gcc-linaro-7.1.1-2017.08-x86_64_aarch64-linux-gnu.tar.xz
-wget $TGZ
-tar -xvf $TGZ
+wget $TXZ
+tar -xvf $TXZ
 
 # gcc-linaro-7.1.1-2017.08-x86_64_aarch64-linux-gnu
 ./gcc-linaro-7.1.1-2017.08-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc --version
