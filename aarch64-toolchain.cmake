63ec6ea24905051a6b755601c3c454e718af59ee
Francesco Petrogalli <francesco.petrogalli@arm.com>
2017-10-12 21:38:35 +0100

    add toolchain file

---
+++ aarch64-toolchain.cmake
@@ -0,0 +1,15 @@
+# this one is important
+SET(CMAKE_SYSTEM_NAME Linux)
+
+# specify the cross compiler
+SET(CMAKE_C_COMPILER   /opt/linaro-toolchain/bin/aarch64-linux-gnu-gcc)
+SET(CMAKE_CXX_COMPILER /opt/linaro-toolchain/bin/aarch64-linux-gnu-g++)
+
+# where is the target environment 
+SET(CMAKE_FIND_ROOT_PATH  /opt/linaro-toolchain/)
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
