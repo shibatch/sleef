0ff74a09e5cc7e6aade712e81839bc3e7651c97d
Martin Krastev <blu.dark@gmail.com>
2017-11-29 10:28:27 +0200

    [optimize] Add single/double precision vmaxnum()/vminnum(). (#109)
    * add single- and double-precision vmaxnum()/vminnum() adhering to IEEE-754-2008 maxNum()/minNum() semantics
    * make xfmaxf(), xfminf(), xfmax() and xfmin() use vmaxnum() and vminnum()
    * add vminnum()/vmaxnum() capabilities to arch SSE2, SSE3, SSE4.1, AVX, AVX2 and AArch64
    * refactor generic vminnum()/vmaxnum() in dd.h and df.h to use the expression from pre-vminnum/vmaxnum xfmin()/xfmax() for better performance

--- src/libm/sleefsimddp.c
+++ src/libm/sleefsimddp.c
@@ -2202,13 +2202,9 @@ EXPORT CONST vdouble xfabs(vdouble x) { return vabs_vd_vd(x); }
 
 EXPORT CONST vdouble xcopysign(vdouble x, vdouble y) { return vcopysign_vd_vd_vd(x, y); }
 
-EXPORT CONST vdouble xfmax(vdouble x, vdouble y) {
-  return vsel_vd_vo_vd_vd(visnan_vo_vd(y), x, vsel_vd_vo_vd_vd(vgt_vo_vd_vd(x, y), x, y));
-}
+EXPORT CONST vdouble xfmax(vdouble x, vdouble y) { return vmaxnum_vd_vd_vd(x, y); }
 
-EXPORT CONST vdouble xfmin(vdouble x, vdouble y) {
-  return vsel_vd_vo_vd_vd(visnan_vo_vd(y), x, vsel_vd_vo_vd_vd(vgt_vo_vd_vd(y, x), x, y));
-}
+EXPORT CONST vdouble xfmin(vdouble x, vdouble y) { return vminnum_vd_vd_vd(x, y); }
 
 EXPORT CONST vdouble xfdim(vdouble x, vdouble y) {
   vdouble ret = vsub_vd_vd_vd(x, y);
