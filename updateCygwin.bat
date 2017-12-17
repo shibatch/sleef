eb2dddcf28b66290cf0356bf6d2323ae28a26295
Naoki Shibata <shibatch@users.sourceforge.net>
2017-12-17 20:29:42 +0900

    no message

--- updateCygwin.bat
+++ updateCygwin.bat
@@ -1,3 +1,4 @@
-C:\Cygwin64\setup-x86_64.exe -q -g -P libmpfr-dev
-C:\Cygwin64\setup-x86_64.exe -q -g -P libgmp-dev
-C:\Cygwin64\setup-x86_64.exe -q -g -P make
+C:\Cygwin64\setup-x86_64.exe -q -g
+C:\Cygwin64\setup-x86_64.exe -q -P libmpfr-dev
+C:\Cygwin64\setup-x86_64.exe -q -P libgmp-dev
+C:\Cygwin64\setup-x86_64.exe -q -P make
