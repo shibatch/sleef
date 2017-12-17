83927b652c2762d7226f645847e03f653f1fed8b
Naoki Shibata <shibatch@users.sourceforge.net>
2017-12-18 01:36:09 +0900

    no message

--- src/script/testOnAppveyor.sh
+++ src/script/testOnAppveyor.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 HOME=/cygdrive/c/projects/sleef
 for i in $HOME/build/bin/Release/iut*.exe; do
-    $HOME/src/libm-tester/test.exe $i || exit 1
+    $HOME/src/libm-tester/tester.exe $i || exit 1
 done
