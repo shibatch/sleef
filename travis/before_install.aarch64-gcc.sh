d5e4ad44b0779e7701aff5d0c30fae150fa6925f
Naoki Shibata <shibatch@users.sourceforge.net>
2018-01-29 20:30:29 +0900

    no message

--- travis/before_install.aarch64-gcc.sh
+++ travis/before_install.aarch64-gcc.sh
@@ -1,2 +1,16 @@
-sudo apt-get -qq update
-sudo apt-get install -y cmake libmpfr-dev
+#!/bin/sh
+cat /etc/apt/sources.list | sed -e 's/^deb /deb \[arch=amd64\] /g' > /tmp/sources.list
+cat <<EOF | sed -e 's/CODENAME/'`lsb_release -s -c`'/g' >> /tmp/sources.list
+deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ CODENAME main restricted
+deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ CODENAME-updates main restricted
+deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ CODENAME universe
+deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ CODENAME-updates universe
+deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ CODENAME-backports main restricted
+deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ CODENAME-security main restricted
+deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ CODENAME-security universe
+deb [arch=arm64] http://ports.ubuntu.com/ubuntu-ports/ CODENAME-security multiverse
+EOF
+mv /tmp/sources.list /etc/apt/sources.list
+dpkg --add-architecture arm64
+apt-get -qq update
+apt-get install cmake gcc-aarch64-linux-gnu libc6-arm64-cross binfmt-support libc6:arm64 libmpfr-dev:arm64 libgomp1:arm64 qemu-user-static
