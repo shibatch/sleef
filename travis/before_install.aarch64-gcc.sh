a481ee9d53b184fae265ccf6e46fb3d78dd4f0f4
Naoki Shibata <shibatch@users.sourceforge.net>
2018-01-30 00:27:11 +0900

    no message

--- travis/before_install.aarch64-gcc.sh
+++ travis/before_install.aarch64-gcc.sh
@@ -22,4 +22,4 @@ deb-src http://ports.ubuntu.com/ubuntu-ports/ CODENAME-security multiverse
 EOF
 mv /tmp/sources.list /etc/apt/sources.list
 apt-get -qq update
-apt-get install git cmake gcc-aarch64-linux-gnu libc6-arm64-cross binfmt-support libc6:arm64 libmpfr-dev:arm64 libgomp1:arm64 qemu-user-static
+apt-get install -y git cmake gcc-aarch64-linux-gnu libc6-arm64-cross binfmt-support libc6:arm64 libmpfr-dev:arm64 libgomp1:arm64 qemu-user-static
