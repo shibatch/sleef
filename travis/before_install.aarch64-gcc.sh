0822044e149feddd1363f89cbf01282a838a08c3
Naoki Shibata <shibatch@users.sourceforge.net>
2018-01-29 23:06:30 +0900

    no message

--- travis/before_install.aarch64-gcc.sh
+++ travis/before_install.aarch64-gcc.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd /build
 dpkg --add-architecture arm64
 cat /etc/apt/sources.list | sed -e 's/^deb /deb \[arch=amd64\] /g' -e 's/\[arch=amd64\] \[arch=amd64\]/\[arch=amd64\]/g' > /tmp/sources.list
 cat <<EOF | sed -e 's/CODENAME/'`lsb_release -s -c`'/g' >> /tmp/sources.list
@@ -22,4 +21,4 @@ deb-src http://ports.ubuntu.com/ubuntu-ports/ CODENAME-security multiverse
 EOF
 mv /tmp/sources.list /etc/apt/sources.list
 apt-get -qq update
-apt-get install cmake gcc-aarch64-linux-gnu libc6-arm64-cross binfmt-support libc6:arm64 libmpfr-dev:arm64 libgomp1:arm64 qemu-user-static
+apt-get install git cmake gcc-aarch64-linux-gnu libc6-arm64-cross binfmt-support libc6:arm64 libmpfr-dev:arm64 libgomp1:arm64 qemu-user-static
