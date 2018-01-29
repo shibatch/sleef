2dcb6687b0167495af5fc3c73a4d112b7ec4dfee
Naoki Shibata <shibatch@users.sourceforge.net>
2018-01-30 01:06:58 +0900

    no message

--- travis/before_script.aarch64-gcc.sh
+++ travis/before_script.aarch64-gcc.sh
@@ -2,4 +2,4 @@
 cd /build
 mkdir sleef.build
 cd sleef.build
-cmake -DCMAKE_TOOLCHAIN_FILE=../travis/toolchain-aarch64.cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../install -DSLEEF_SHOW_CONFIG=1 ..
+cmake -DCMAKE_TOOLCHAIN_FILE=../travis/toolchain-aarch64.cmake -DEMULATOR=qemu-aarch64-static -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../install -DSLEEF_SHOW_CONFIG=1 ..
