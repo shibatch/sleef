e78d2fe280f8a8647831be54ab9a9566878d007b
Naoki Shibata <shibatch@users.sourceforge.net>
2018-01-30 12:44:27 +0900

    no message

---
+++ travis/before_script.armhf-gcc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -ev
+cd /build
+mkdir build-native
+cd build-native
+cmake ..
+make -j 2 all
+cd /build
+mkdir build-cross
+cd build-cross
+cmake -DCMAKE_TOOLCHAIN_FILE=../travis/toolchain-armhf.cmake -DNATIVE_BUILD_DIR=`pwd`/../build-native -DEMULATOR=qemu-arm-static -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../install -DSLEEF_SHOW_CONFIG=1 ..
