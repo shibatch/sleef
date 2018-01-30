d408c89aa6bca4d9fe2533ea53e68de0d73a9582
Naoki Shibata <shibatch@users.sourceforge.net>
2018-01-30 14:01:13 +0900

    no message

--- travis/before_install.x86_64-clang.sh
+++ travis/before_install.x86_64-clang.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -ev
-export PATH=$PATH:/usr/bin:`pwd`/sde-external-8.12.0-2017-10-23-lin
 #tar xf sde-external-8.12.0-2017-10-23-lin.tar.bz2 # Turned off SDE to reduce time for testing
 sudo add-apt-repository -y ppa:adrozdoff/cmake
 sudo apt-get -qq update
