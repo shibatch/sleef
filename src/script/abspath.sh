82e44e92026d533606e9bc4a7a075b12ed335a67
Naoki Shibata <shibatch@users.sourceforge.net>
2018-02-21 18:32:04 +0900

    [cmake] Remove makefiles (#175)
    This terminates the migration work mentioned in https://github.com/shibatch/sleef/issues/30

--- src/script/abspath.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ `uname` = Darwin ]; then
-    find `cd $1; pwd` -name $2 -print
-elif [ `uname -o` = Cygwin ]; then
-    find `cd $1; pwd` -name $2 -exec cygpath -w -p -a {} \;
-else
-    find `cd $1; pwd` -name $2 -print
-fi
