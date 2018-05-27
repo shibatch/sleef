08317d608a5761be7351653005fa8785dba8ac29
Naoki Shibata <shibatch@users.sourceforge.net>
2018-05-27 20:48:22 +0900

    no message

---
+++ travis/before_install.ppc64el-clang.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -ev
+export PATH=$PATH:/usr/bin
+dpkg --add-architecture ppc64el
+cat /etc/apt/sources.list | sed -e 's/^deb /deb \[arch=ppc64el\] /g' -e 's/\[arch=ppc64el\] \[arch=ppc64el\]/\[arch=ppc64el\]/g' > /tmp/sources.list
+cat <<EOF | sed -e 's/CODENAME/xenial/g' >> /tmp/sources.list
+deb [arch=ppc64el] http://ports.ubuntu.com/ubuntu-ports/ CODENAME main restricted
+deb-src http://ports.ubuntu.com/ubuntu-ports/ CODENAME main restricted
+deb [arch=ppc64el] http://ports.ubuntu.com/ubuntu-ports/ CODENAME-updates main restricted
+deb-src http://ports.ubuntu.com/ubuntu-ports/ CODENAME-updates main restricted
+deb [arch=ppc64el] http://ports.ubuntu.com/ubuntu-ports/ CODENAME universe
+deb-src http://ports.ubuntu.com/ubuntu-ports/ CODENAME universe
+deb [arch=ppc64el] http://ports.ubuntu.com/ubuntu-ports/ CODENAME-updates universe
+deb-src http://ports.ubuntu.com/ubuntu-ports/ CODENAME-updates universe
+deb [arch=ppc64el] http://ports.ubuntu.com/ubuntu-ports/ CODENAME-backports main restricted
+deb-src http://ports.ubuntu.com/ubuntu-ports/ CODENAME-backports main restricted
+deb [arch=ppc64el] http://ports.ubuntu.com/ubuntu-ports/ CODENAME-security main restricted
+deb-src http://ports.ubuntu.com/ubuntu-ports/ CODENAME-security main restricted
+deb [arch=ppc64el] http://ports.ubuntu.com/ubuntu-ports/ CODENAME-security universe
+deb-src http://ports.ubuntu.com/ubuntu-ports/ CODENAME-security universe
+deb [arch=ppc64el] http://ports.ubuntu.com/ubuntu-ports/ CODENAME-security multiverse
+deb-src http://ports.ubuntu.com/ubuntu-ports/ CODENAME-security multiverse
+EOF
+mv /tmp/sources.list /etc/apt/sources.list
+apt-get -qq update
+apt-get install -y git cmake clang-5.0 libc6-ppc64el-cross libc6:ppc64el libmpfr-dev:ppc64el libgomp1:ppc64el libmpfr-dev binfmt-support qemu qemu-user-static libfftw3-dev:ppc64el
---
+++ travis/before_script.ppc64el-clang.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ev
+cd /build
+mkdir build-native
+cd build-native
+cmake ..
+make -j 2 all
+cd /build
+mkdir bin
+cat <<EOF > /build/bin/ppc64el-cc
+clang-5.0 -target ppc64el-linux-gnu -mvsx $*
+EOF
+chmod +x /build/bin/ppc64el-cc
+export PATH=$PATH:/build/bin
+mkdir build-cross
+cd build-cross
+cmake -DCMAKE_TOOLCHAIN_FILE=../travis/toolchain-ppc64el.cmake -DNATIVE_BUILD_DIR=`pwd`/../build-native -DEMULATOR=qemu-ppc64el-static -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../install -DSLEEF_SHOW_CONFIG=1 ..
