d7c1457c96bf1063d04dc4c55841d660915668b9
Naoki Shibata <shibatch@users.sourceforge.net>
2018-05-27 22:41:22 +0900

    no message

--- travis/before_install.ppc64el-clang.sh
+++ travis/before_install.ppc64el-clang.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ev
 export PATH=$PATH:/usr/bin
-apt-get -qq update
-apt-get install -y wget
+#apt-get -qq update
+#apt-get install -y wget
 dpkg --add-architecture ppc64el
 cat /etc/apt/sources.list | sed -e 's/^deb /deb \[arch=amd64\] /g' -e 's/\[arch=amd64\] \[arch=amd64\]/\[arch=amd64\]/g' > /tmp/sources.list
 cat <<EOF | sed -e 's/CODENAME/xenial/g' >> /tmp/sources.list
@@ -22,9 +22,9 @@ deb [arch=ppc64el] http://ports.ubuntu.com/ubuntu-ports/ CODENAME-security unive
 deb-src http://ports.ubuntu.com/ubuntu-ports/ CODENAME-security universe
 deb [arch=ppc64el] http://ports.ubuntu.com/ubuntu-ports/ CODENAME-security multiverse
 deb-src http://ports.ubuntu.com/ubuntu-ports/ CODENAME-security multiverse
-deb http://apt.llvm.org/xenial/ llvm-toolchain-CODENAME-6.0 main
+#deb http://apt.llvm.org/xenial/ llvm-toolchain-CODENAME-6.0 main
 EOF
 mv /tmp/sources.list /etc/apt/sources.list
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|apt-key add -
+#wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|apt-key add -
 apt-get -qq update
-apt-get install -y git cmake clang-6.0 gcc-powerpc64le-linux-gnu libc6-ppc64el-cross libc6:ppc64el libmpfr-dev:ppc64el libgomp1:ppc64el libmpfr-dev binfmt-support qemu qemu-user-static libfftw3-dev:ppc64el
+apt-get install -y git cmake clang-5.0 gcc-powerpc64le-linux-gnu libc6-ppc64el-cross libc6-dev:ppc64el libmpfr-dev:ppc64el libgomp1:ppc64el libmpfr-dev binfmt-support qemu qemu-user-static libfftw3-dev:ppc64el
