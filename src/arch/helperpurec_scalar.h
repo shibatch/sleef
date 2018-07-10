0473f8938d5ea1afa5e2123973b38fc2640a0860
Naoki Shibata <shibatch@users.sourceforge.net>
2018-07-10 19:25:47 +0900

    no message

--- src/arch/helperpurec_scalar.h
+++ src/arch/helperpurec_scalar.h
@@ -29,7 +29,16 @@
 #define ENABLE_FMA_DP
 #define ENABLE_FMA_SP
 
-#if !defined(__AVX2__) && !defined(__VSX__) && (!defined(FP_FAST_FMA) || !defined(FP_FAST_FMAF))
+#if defined(__AVX2__) || defined(__VSX__) || defined(__amd64__) || defined(__arm__)
+#ifndef FP_FAST_FMA
+#define FP_FAST_FMA
+#endif
+#ifndef FP_FAST_FMAF
+#define FP_FAST_FMAF
+#endif
+#endif
+
+#if !defined(FP_FAST_FMA) || !defined(FP_FAST_FMAF)
 #error FP_FAST_FMA or FP_FAST_FMAF not defined
 #endif
 #define ISANAME "Pure C scalar with FMA"
