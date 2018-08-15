ede3474fe759073140b611cee4315d44870d73ae
Naoki Shibata <shibatch@users.sourceforge.net>
2018-08-15 15:26:05 +0900

    no message

--- Configure.cmake
+++ Configure.cmake
@@ -247,24 +247,54 @@ endif()
 
 # FEATURE DETECTION
 
-CHECK_TYPE_SIZE("long double" LD_SIZE)
-if(LD_SIZE GREATER "9")
-  # This is needed to check since internal compiler error occurs with gcc 4.x
-  CHECK_C_SOURCE_COMPILES("
+# Long double
+
+option(DISABLE_LONG_DOUBLE "Disable long double" OFF)
+option(ENFORCE_LONG_DOUBLE "Build fails if long double is not supported by the compiler" OFF)
+
+if(NOT DISABLE_LONG_DOUBLE)
+  CHECK_TYPE_SIZE("long double" LD_SIZE)
+  if(LD_SIZE GREATER "9")
+    # This is needed to check since internal compiler error occurs with gcc 4.x
+    CHECK_C_SOURCE_COMPILES("
   typedef long double vlongdouble __attribute__((vector_size(sizeof(long double)*2)));
   vlongdouble vcast_vl_l(long double d) { return (vlongdouble) { d, d }; }
   int main() { vlongdouble vld = vcast_vl_l(0);
   }" COMPILER_SUPPORTS_LONG_DOUBLE)
+  endif()
+else()
+  message(STATUS "Support for long double disabled by CMake option")
 endif()
 
-CHECK_C_SOURCE_COMPILES("
+if (ENFORCE_LONG_DOUBLE AND NOT COMPILER_SUPPORTS_LONG_DOUBLE)
+  message(FATAL_ERROR "ENFORCE_LONG_DOUBLE is specified and that feature is disabled or not supported by the compiler")
+endif()
+
+# float128
+
+option(DISABLE_FLOAT128 "Disable float128" OFF)
+option(ENFORCE_FLOAT128 "Build fails if float128 is not supported by the compiler" OFF)
+
+if(NOT DISABLE_FLOAT128)
+  CHECK_C_SOURCE_COMPILES("
   int main() { __float128 r = 1;
   }" COMPILER_SUPPORTS_FLOAT128)
+else()
+  message(STATUS "Support for float128 disabled by CMake option")
+endif()
 
-# Detect if sleef supported architectures are also supported by the compiler
+if (ENFORCE_FLOAT128 AND NOT COMPILER_SUPPORTS_FLOAT128)
+  message(FATAL_ERROR "ENFORCE_FLOAT128 is specified and that feature is disabled or not supported by the compiler")
+endif()
 
-set (CMAKE_REQUIRED_FLAGS ${FLAGS_ENABLE_SSE2})
-CHECK_C_SOURCE_COMPILES("
+# SSE2
+
+option(DISABLE_SSE2 "Disable SSE2" OFF)
+option(ENFORCE_SSE2 "Build fails if SSE2 is not supported by the compiler" OFF)
+
+if(SLEEF_ARCH_X86 AND NOT DISABLE_SSE2)
+  set (CMAKE_REQUIRED_FLAGS ${FLAGS_ENABLE_SSE2})
+  CHECK_C_SOURCE_COMPILES("
   #if defined(_MSC_VER)
   #include <intrin.h>
   #else
@@ -272,10 +302,21 @@ CHECK_C_SOURCE_COMPILES("
   #endif
   int main() {
     __m128d r = _mm_mul_pd(_mm_set1_pd(1), _mm_set1_pd(2)); }"
-  COMPILER_SUPPORTS_SSE2)
+    COMPILER_SUPPORTS_SSE2)
+endif()
 
-set (CMAKE_REQUIRED_FLAGS ${FLAGS_ENABLE_SSE4})
-CHECK_C_SOURCE_COMPILES("
+if (ENFORCE_SSE2 AND NOT COMPILER_SUPPORTS_SSE2)
+  message(FATAL_ERROR "ENFORCE_SSE2 is specified and that feature is disabled or not supported by the compiler")
+endif()
+
+# SSE 4.1
+
+option(DISABLE_SSE4 "Disable SSE4" OFF)
+option(ENFORCE_SSE4 "Build fails if SSE4 is not supported by the compiler" OFF)
+
+if(SLEEF_ARCH_X86 AND NOT DISABLE_SSE4)
+  set (CMAKE_REQUIRED_FLAGS ${FLAGS_ENABLE_SSE4})
+  CHECK_C_SOURCE_COMPILES("
   #if defined(_MSC_VER)
   #include <intrin.h>
   #else
@@ -283,10 +324,21 @@ CHECK_C_SOURCE_COMPILES("
   #endif
   int main() {
     __m128d r = _mm_floor_sd(_mm_set1_pd(1), _mm_set1_pd(2)); }"
-  COMPILER_SUPPORTS_SSE4)
+    COMPILER_SUPPORTS_SSE4)
+endif()
 
-set (CMAKE_REQUIRED_FLAGS ${FLAGS_ENABLE_AVX})
-CHECK_C_SOURCE_COMPILES("
+if (ENFORCE_SSE4 AND NOT COMPILER_SUPPORTS_SSE4)
+  message(FATAL_ERROR "ENFORCE_SSE4 is specified and that feature is disabled or not supported by the compiler")
+endif()
+
+# AVX
+
+option(DISABLE_SSE4 "Disable AVX" OFF)
+option(ENFORCE_SSE4 "Build fails if AVX is not supported by the compiler" OFF)
+
+if(SLEEF_ARCH_X86 AND NOT DISABLE_AVX)
+  set (CMAKE_REQUIRED_FLAGS ${FLAGS_ENABLE_AVX})
+  CHECK_C_SOURCE_COMPILES("
   #if defined(_MSC_VER)
   #include <intrin.h>
   #else
@@ -295,9 +347,20 @@ CHECK_C_SOURCE_COMPILES("
   int main() {
     __m256d r = _mm256_add_pd(_mm256_set1_pd(1), _mm256_set1_pd(2));
   }" COMPILER_SUPPORTS_AVX)
+endif()
 
-set (CMAKE_REQUIRED_FLAGS ${FLAGS_ENABLE_FMA4})
-CHECK_C_SOURCE_COMPILES("
+if (ENFORCE_AVX AND NOT COMPILER_SUPPORTS_AVX)
+  message(FATAL_ERROR "ENFORCE_AVX is specified and that feature is disabled or not supported by the compiler")
+endif()
+
+# FMA4
+
+option(DISABLE_FMA4 "Disable FMA4" OFF)
+option(ENFORCE_FMA4 "Build fails if FMA4 is not supported by the compiler" OFF)
+
+if(SLEEF_ARCH_X86 AND NOT DISABLE_FMA4)
+  set (CMAKE_REQUIRED_FLAGS ${FLAGS_ENABLE_FMA4})
+  CHECK_C_SOURCE_COMPILES("
   #if defined(_MSC_VER)
   #include <intrin.h>
   #else
@@ -305,10 +368,21 @@ CHECK_C_SOURCE_COMPILES("
   #endif
   int main() {
     __m256d r = _mm256_macc_pd(_mm256_set1_pd(1), _mm256_set1_pd(2), _mm256_set1_pd(3)); }"
-  COMPILER_SUPPORTS_FMA4)
+    COMPILER_SUPPORTS_FMA4)
+endif()
 
-set (CMAKE_REQUIRED_FLAGS ${FLAGS_ENABLE_AVX2})
-CHECK_C_SOURCE_COMPILES("
+if (ENFORCE_FMA4 AND NOT COMPILER_SUPPORTS_FMA4)
+  message(FATAL_ERROR "ENFORCE_FMA4 is specified and that feature is disabled or not supported by the compiler")
+endif()
+
+# AVX2
+
+option(DISABLE_AVX2 "Disable AVX2" OFF)
+option(ENFORCE_AVX2 "Build fails if AVX2 is not supported by the compiler" OFF)
+
+if(SLEEF_ARCH_X86 AND NOT DISABLE_AVX2)
+  set (CMAKE_REQUIRED_FLAGS ${FLAGS_ENABLE_AVX2})
+  CHECK_C_SOURCE_COMPILES("
   #if defined(_MSC_VER)
   #include <intrin.h>
   #else
@@ -316,17 +390,26 @@ CHECK_C_SOURCE_COMPILES("
   #endif
   int main() {
     __m256i r = _mm256_abs_epi32(_mm256_set1_epi32(1)); }"
-  COMPILER_SUPPORTS_AVX2)
+    COMPILER_SUPPORTS_AVX2)
 
-set (CMAKE_REQUIRED_FLAGS ${FLAGS_ENABLE_SVE})
-CHECK_C_SOURCE_COMPILES("
-  #include <arm_sve.h>
-  int main() {
-    svint32_t r = svdup_n_s32(1); }"
-  COMPILER_SUPPORTS_SVE)
+  # AVX2 implies AVX2128
+  if(COMPILER_SUPPORTS_AVX2)
+    set(COMPILER_SUPPORTS_AVX2128 1)
+  endif()
+endif()
 
-set (CMAKE_REQUIRED_FLAGS ${FLAGS_ENABLE_AVX512F})
-CHECK_C_SOURCE_COMPILES("
+if (ENFORCE_AVX2 AND NOT COMPILER_SUPPORTS_AVX2)
+  message(FATAL_ERROR "ENFORCE_AVX2 is specified and that feature is disabled or not supported by the compiler")
+endif()
+
+# AVX512F
+
+option(DISABLE_AVX512F "Disable AVX512F" OFF)
+option(ENFORCE_AVX512F "Build fails if AVX512F is not supported by the compiler" OFF)
+
+if(SLEEF_ARCH_X86 AND NOT DISABLE_AVX512F)
+  set (CMAKE_REQUIRED_FLAGS ${FLAGS_ENABLE_AVX512F})
+  CHECK_C_SOURCE_COMPILES("
   #if defined(_MSC_VER)
   #include <intrin.h>
   #else
@@ -340,38 +423,81 @@ CHECK_C_SOURCE_COMPILES("
     __m256i ymm = _mm512_extracti64x4_epi64(a, 0);
     __mmask16 m = _mm512_cmp_epi32_mask(a, a, _MM_CMPINT_EQ);
     __m512i r = _mm512_andnot_si512(a, a); }"
-  COMPILER_SUPPORTS_AVX512F)
-
-# AVX2 implies AVX2128
-if(COMPILER_SUPPORTS_AVX2)
-  set(COMPILER_SUPPORTS_AVX2128 1)
+    COMPILER_SUPPORTS_AVX512F)
 endif()
 
-set (CMAKE_REQUIRED_FLAGS ${FLAGS_ENABLE_VSX})
-CHECK_C_SOURCE_COMPILES("
+if (ENFORCE_AVX512F AND NOT COMPILER_SUPPORTS_AVX512F)
+  message(FATAL_ERROR "ENFORCE_AVX512F is specified and that feature is disabled or not supported by the compiler")
+endif()
+
+# SVE
+
+option(DISABLE_SVE "Disable SVE" OFF)
+option(ENFORCE_SVE "Build fails if SVE is not supported by the compiler" OFF)
+
+if(SLEEF_ARCH_AARCH64 AND NOT DISABLE_SVE)
+  set (CMAKE_REQUIRED_FLAGS ${FLAGS_ENABLE_SVE})
+  CHECK_C_SOURCE_COMPILES("
+  #include <arm_sve.h>
+  int main() {
+    svint32_t r = svdup_n_s32(1); }"
+    COMPILER_SUPPORTS_SVE)
+endif()
+
+if (ENFORCE_SVE AND NOT COMPILER_SUPPORTS_SVE)
+  message(FATAL_ERROR "ENFORCE_SVE is specified and that feature is disabled or not supported by the compiler")
+endif()
+
+# VSX
+
+option(DISABLE_VSX "Disable VSX" OFF)
+option(ENFORCE_VSX "Build fails if VSX is not supported by the compiler" OFF)
+
+if(SLEEF_ARCH_PPC64 AND NOT DISABLE_VSX)
+  set (CMAKE_REQUIRED_FLAGS ${FLAGS_ENABLE_VSX})
+  CHECK_C_SOURCE_COMPILES("
   #include <altivec.h>
   int main() {
     vector double d;
     d = vec_perm(d, d, (vector unsigned char)(4, 5, 6, 7, 0, 1, 2, 3, 12, 13, 14, 15, 8, 9, 10, 11));
   }"
-  COMPILER_SUPPORTS_VSX)
+    COMPILER_SUPPORTS_VSX)
+endif()
 
-# Check if compilation with OpenMP really succeeds
-# It does not succeed on Travis even though find_package(OpenMP) succeeds.
-find_package(OpenMP)
-if(OPENMP_FOUND)
-  set (CMAKE_REQUIRED_FLAGS "${OpenMP_C_FLAGS}")
-  CHECK_C_SOURCE_COMPILES("
+if (ENFORCE_VSX AND NOT COMPILER_SUPPORTS_VSX)
+  message(FATAL_ERROR "ENFORCE_VSX is specified and that feature is disabled or not supported by the compiler")
+endif()
+
+# OpenMP
+
+option(DISABLE_OPENMP "Disable OPENMP" OFF)
+option(ENFORCE_OPENMP "Build fails if OPENMP is not supported by the compiler" OFF)
+
+if(NOT DISABLE_OPENMP)
+  find_package(OpenMP)
+  # Check if compilation with OpenMP really succeeds
+  # It does not succeed on Travis even though find_package(OpenMP) succeeds.
+  if(OPENMP_FOUND)
+    set (CMAKE_REQUIRED_FLAGS "${OpenMP_C_FLAGS}")
+    CHECK_C_SOURCE_COMPILES("
   #include <stdio.h>
   int main() {
   int i;
   #pragma omp parallel for
     for(i=0;i < 10;i++) { putchar(0); }
   }"
-  COMPILER_SUPPORTS_OPENMP)
-endif(OPENMP_FOUND)
+      COMPILER_SUPPORTS_OPENMP)
+  endif(OPENMP_FOUND)
+else()
+  message(STATUS "Support for OpenMP disabled by CMake option")
+endif()
 
-# Check weak aliases are supported.
+if (ENFORCE_OPENMP AND NOT COMPILER_SUPPORTS_OPENMP)
+  message(FATAL_ERROR "ENFORCE_OPENMP is specified and that feature is disabled or not supported by the compiler")
+endif()
+
+# Weak aliases
+
 CHECK_C_SOURCE_COMPILES("
 #if defined(__CYGWIN__)
 #define EXPORT __stdcall __declspec(dllexport)
@@ -393,6 +519,8 @@ if (COMPILER_SUPPORTS_WEAK_ALIASES AND
   set(ENABLE_GNUABI ${COMPILER_SUPPORTS_WEAK_ALIASES})
 endif()
 
+# Built-in math functions
+
 CHECK_C_SOURCE_COMPILES("
   int main(void) {
     double a = __builtin_sqrt (2);
@@ -400,11 +528,28 @@ CHECK_C_SOURCE_COMPILES("
   }"
   COMPILER_SUPPORTS_BUILTIN_MATH)
 
+# SYS_getrandom
+
+CHECK_C_SOURCE_COMPILES("
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <linux/random.h>
+  int main(void) {
+    int i;
+    syscall(SYS_getrandom, &i, sizeof(i), 0);
+  }"
+  COMPILER_SUPPORTS_SYS_GETRANDOM)
+
+#
+
 # Reset used flags
 set(CMAKE_REQUIRED_FLAGS)
 
 # Save the default C flags
 set(ORG_CMAKE_C_FLAGS CMAKE_C_FLAGS)
+
+##
 
 # Check if sde64 command is available
 
@@ -419,6 +564,7 @@ find_program(ARMIE_COMMAND armie)
 if (NOT SVE_VECTOR_BITS)
   set(SVE_VECTOR_BITS 128)
 endif()
+
 ##
 
 if(SLEEF_SHOW_ERROR_LOG)
@@ -432,7 +578,7 @@ endif(SLEEF_SHOW_ERROR_LOG)
 string(COMPARE NOTEQUAL "" "$ENV{TRAVIS}" RUNNING_ON_TRAVIS)
 
 if (${RUNNING_ON_TRAVIS} AND CMAKE_C_COMPILER_ID MATCHES "Clang")
-  message("Travix bug workaround turned on")
+  message(STATUS "Travis bug workaround turned on")
   set(COMPILER_SUPPORTS_OPENMP FALSE)   # Workaround for https://github.com/travis-ci/travis-ci/issues/8613
   set(COMPILER_SUPPORTS_FLOAT128 FALSE) # Compilation on unroll_0_vecextqp.c does not finish on Travis
 endif()
@@ -440,6 +586,8 @@ endif()
 if (MSVC)
   set(COMPILER_SUPPORTS_OPENMP FALSE)   # At this time, OpenMP is not supported on MSVC
 endif()
+
+##
 
 # Set common definitions
 
@@ -454,4 +602,8 @@ endif()
 # When cross compiling for ppc64, this bug-workaround is needed
 if(CMAKE_CROSSCOMPILING AND CMAKE_SYSTEM_PROCESSOR MATCHES "^(powerpc|ppc)64")
   set(COMMON_TARGET_DEFINITIONS ${COMMON_TARGET_DEFINITIONS} POWER64_UNDEF_USE_EXTERN_INLINES=1)
+endif()
+
+if (COMPILER_SUPPORTS_SYS_GETRANDOM)
+  set(COMMON_TARGET_DEFINITIONS ${COMMON_TARGET_DEFINITIONS} ENABLE_SYS_getrandom=1)
 endif()
