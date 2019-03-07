aa52a314179be919d4cfdc8dc96d27633227c5db
Naoki Shibata <shibatch@users.sourceforge.net>
2019-03-07 16:27:30 +0900

    no message

--- src/dft-tester/naivetest.c
+++ src/dft-tester/naivetest.c
@@ -1,4 +1,4 @@
-//          Copyright Naoki Shibata 2010 - 2018.
+//          Copyright Naoki Shibata 2010 - 2019.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
