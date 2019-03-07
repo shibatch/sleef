534fe402ba823611a7050925418e02c0ec21e4d3
Naoki Shibata <shibatch@users.sourceforge.net>
2019-03-07 16:31:38 +0900

    no message

--- src/arch/helperadvsimd.h
+++ src/arch/helperadvsimd.h
@@ -1,5 +1,5 @@
 /*********************************************************************/
-/*          Copyright ARM Ltd. 2010 - 2017.                          */
+/*          Copyright ARM Ltd. 2010 - 2019.                          */
 /* Distributed under the Boost Software License, Version 1.0.        */
 /*    (See accompanying file LICENSE.txt or copy at                  */
 /*          http://www.boost.org/LICENSE_1_0.txt)                    */
