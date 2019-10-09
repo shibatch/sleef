e0fd6990efc0255566a885a395cdbfa8d479d2b4
Naoki Shibata <shibatch@users.sourceforge.net>
2019-10-09 11:48:26 +0900

    no message

--- travis/before_script.arm64-clang.sh
+++ travis/before_script.arm64-clang.sh
@@ -2,6 +2,6 @@
 set -ev
 mkdir sleef.build
 cd sleef.build
-export CC=clang-8.0
-export CXX=clang++-8.0
+export CC=clang-8
+export CXX=clang++-8
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../install -DSLEEF_SHOW_CONFIG=1 -DENFORCE_TESTER3=TRUE -DBUILD_QUAD=TRUE ..
