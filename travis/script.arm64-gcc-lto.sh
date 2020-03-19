b7753728155659d986e90fa8a62549b71d13771f
shibatch <shibatch.sf.net@gmail.com>
2020-03-19 13:16:34 +0900

    no message

--- travis/script.arm64-gcc-lto.sh
+++ travis/script.arm64-gcc-lto.sh
@@ -4,5 +4,6 @@ cd sleef.build
 make -j `nproc` all
 export OMP_WAIT_POLICY=passive
 export CTEST_OUTPUT_ON_FAILURE=TRUE
+readelf -a lib/libsleef.a|grep -c __gnu_lto || true
 ctest -j `nproc`
 make install
