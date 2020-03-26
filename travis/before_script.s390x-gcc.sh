7dbca7cd7a139de301ce23edf68c624b6a7fa4cf
shibatch <shibatch.sf.net@gmail.com>
2020-03-26 14:19:59 +0900

    no message

--- travis/before_script.s390x-gcc.sh
+++ travis/before_script.s390x-gcc.sh
@@ -2,6 +2,6 @@
 set -ev
 mkdir sleef.build
 cd sleef.build
-export CC=gcc-7
-export CXX=g++-7
+export CC=gcc-8
+export CXX=g++-8
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../install -DSLEEF_SHOW_CONFIG=1 -DENFORCE_TESTER3=TRUE ..
