0f8f1d7f2888319046f6a93d3651bb176ea97d2f
shibatch <shibatch.sf.net@gmail.com>
2020-03-28 13:52:37 +0900

    no message

--- travis/before_install.arm64-gcc.sh
+++ travis/before_install.arm64-gcc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ev
 sudo apt-get -qq update
-sudo apt-get install -y cmake libmpfr-dev libssl-dev libfftw3-dev ninja
+sudo apt-get install -y cmake libmpfr-dev libssl-dev libfftw3-dev ninja-build
