e6635b51fc619ba1bdd4215fed31798825663371
Naoki Shibata <shibatch@users.sourceforge.net>
2020-04-02 13:31:00 +0900

    Update CI settings (#294)
    This patch updates CI configurations.
    Ubuntu bionic and newer compilers will be used for testing at Travis.
    Appveyor will utilize VS2019 and self-hosted computers.
    Ninja build system is used in replacement of make.
    
    Authored-by: shibatch <shibatch.sf.net@gmail.com>

--- travis/before_install.osx-gcc.sh
+++ travis/before_install.osx-gcc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ev
 brew update
-brew install gcc@6 openssl
+brew install gcc@6 openssl ninja
--- travis/before_script.osx-clang.sh
+++ travis/before_script.osx-clang.sh
@@ -2,4 +2,4 @@
 set -ev
 mkdir sleef.build
 cd sleef.build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../install -DSLEEF_SHOW_CONFIG=1 -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DENFORCE_TESTER3=TRUE -DBUILD_QUAD=TRUE ..
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../install -DSLEEF_SHOW_CONFIG=1 -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DENFORCE_TESTER3=TRUE -DBUILD_QUAD=TRUE ..
