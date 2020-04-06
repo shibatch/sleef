5d6be85161dbed06c4147c917277c8f1af8867db
shibatch <shibatch.sf.net@gmail.com>
2020-04-06 11:07:37 +0900

    no message

--- src/libm-tester/iutsimdmain.c
+++ src/libm-tester/iutsimdmain.c
@@ -15,7 +15,7 @@ int do_test(int argc, char **argv);
 int check_featureDP(double d);
 int check_featureSP(float d);
 
-#ifdef _MSC_VER
+#if defined(_MSC_VER) || defined(__MINGW32__) || defined(__MINGW64__)
 #define SETJMP(x) setjmp(x)
 #define LONGJMP longjmp
 #else
