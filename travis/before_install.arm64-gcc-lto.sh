ea6bd9a05d260742d4451646262ed0080d65411f
shibatch <shibatch.sf.net@gmail.com>
2020-04-08 08:37:53 +0900

    no message

--- travis/before_install.arm64-gcc-lto.sh
+++ travis/before_install.arm64-gcc-lto.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ev
 sudo apt-get -qq update
-sudo apt-get install -y cmake libmpfr-dev libssl-dev libfftw3-dev
+sudo apt-get install -y cmake ninja-build libmpfr-dev libssl-dev libfftw3-dev
