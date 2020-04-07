368f8d5ed1c4ea7121073961deac0135f9c78d74
shibatch <shibatch.sf.net@gmail.com>
2020-04-08 08:25:35 +0900

    no message

--- travis/script.arm64-clang-lto.sh
+++ travis/script.arm64-clang-lto.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ev
 cd sleef.build
-make -j `nproc` all
+ninja all
 export OMP_WAIT_POLICY=passive
 export CTEST_OUTPUT_ON_FAILURE=TRUE
 test -f lib/sleefdp.ll
 ctest -j `nproc`
-make install
+ninja install
--- travis/script.arm64-gcc-lto.sh
+++ travis/script.arm64-gcc-lto.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ev
 cd sleef.build
-make -j `nproc` all
+ninja all
 export OMP_WAIT_POLICY=passive
 export CTEST_OUTPUT_ON_FAILURE=TRUE
 readelf -a lib/libsleef.a|grep -cq __gnu_lto
 ctest -j `nproc`
-make install
+ninja install
