01d87a629dd5ad4297d362f218b4edc1f648d691
shibatch <shibatch.sf.net@gmail.com>
2020-05-29 11:43:22 +0900

    no message

--- travis/before_script.arm64-gcc.sh
+++ travis/before_script.arm64-gcc.sh
@@ -2,5 +2,5 @@
 set -ev
 mkdir sleef.build
 cd sleef.build
-export CC=gcc-8
+export CC=gcc-10
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../install -DSLEEF_SHOW_CONFIG=1 -DENFORCE_TESTER3=TRUE -DBUILD_QUAD=TRUE ..
