9524450a2d724dcbfcd2f5df2a330ca0068771b9
shibatch <shibatch.sf.net@gmail.com>
2020-05-31 15:23:25 +0900

    no message

--- travis/before_install.arm64-gcc-sve.sh
+++ travis/before_install.arm64-gcc-sve.sh
@@ -2,5 +2,5 @@
 set -ev
 sudo apt-get -qq update
 sudo apt-get install -y cmake libmpfr-dev libssl-dev libfftw3-dev ninja-build
-wget https://shibata.naist.jp/~n-sibata/travis/binutils-2.34-aarch64.tar.xz
-tar xvf binutils-2.34-aarch64.tar.xz -C /
+wget -q https://shibata.naist.jp/~n-sibata/travis/binutils-2.34-aarch64.tar.xz
+tar xv binutils-2.34-aarch64.tar.xz -C /
