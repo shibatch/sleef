a5f2535184c77b9f0d44a5c6a4d8bcca49c6c7b9
shibatch <shibatch.sf.net@gmail.com>
2021-01-15 20:35:45 +0900

    no message

--- src/arch/helperpurec_scalar.h
+++ src/arch/helperpurec_scalar.h
@@ -121,7 +121,30 @@ typedef struct {
   vmask x, y;
 } vquad;
 
+#if (defined(__SIZEOF_FLOAT128__) && __SIZEOF_FLOAT128__ == 16) || (defined(__linux__) && defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))) || (defined(__PPC64__) && defined(__GNUC__) && !defined(__clang__) && __GNUC__ >= 8)
+#define SLEEF_FLOAT128_IS_IEEEQP
+#endif
+
+#if !defined(SLEEF_FLOAT128_IS_IEEEQP) && defined(__SIZEOF_LONG_DOUBLE__) && __SIZEOF_LONG_DOUBLE__ == 16 && (defined(__aarch64__) || defined(__zarch__))
+#define SLEEF_LONGDOUBLE_IS_IEEEQP
+#endif
+
+#if !defined(Sleef_quad_DEFINED)
+#define Sleef_quad_DEFINED
+#if defined(SLEEF_FLOAT128_IS_IEEEQP)
+typedef __float128 Sleef_quad;
+#elif defined(SLEEF_LONGDOUBLE_IS_IEEEQP)
+typedef long double Sleef_quad;
+#else
+typedef vquad Sleef_quad;
+#endif
+#endif
+
+#if CONFIG != 3
 typedef Sleef_quad vargquad;
+#else
+typedef vquad vargquad;
+#endif
 
 //
 
