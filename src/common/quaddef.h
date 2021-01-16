774a9840f617b85d04e3ebab3df9ccae4b3297d7
shibatch <shibatch.sf.net@gmail.com>
2021-01-16 16:16:31 +0900

    no message

--- src/common/quaddef.h
+++ src/common/quaddef.h
@@ -29,23 +29,23 @@ typedef Sleef_uint64_2t Sleef_quad;
 
 #else // #if !defined(SLEEF_GENHEADER)
 
-&if (defined(SLEEFXXX__SIZEOF_FLOAT128__XXX) && SLEEFXXX__SIZEOF_FLOAT128__XXX == 16) || (defined(SLEEFXXX__linux__XXX) && defined(SLEEFXXX__GNUC__XXX) && (defined(SLEEFXXX__i386__XXX) || defined(SLEEFXXX__x86_64__XXX))) || (defined(SLEEFXXX__PPC64__XXX) && defined(SLEEFXXX__GNUC__XXX) && !defined(SLEEFXXX__clang__XXX) && SLEEFXXX__GNUC__XXX >= 8)
-&define SLEEFXXXSLEEF_FLOAT128_IS_IEEEQPXXX
+&if (defined(SLEEFXXX__SIZEOF_FLOAT128__) && SLEEFXXX__SIZEOF_FLOAT128__ == 16) || (defined(SLEEFXXX__linux__) && defined(SLEEFXXX__GNUC__) && (defined(SLEEFXXX__i386__) || defined(SLEEFXXX__x86_64__))) || (defined(SLEEFXXX__PPC64__) && defined(SLEEFXXX__GNUC__) && !defined(SLEEFXXX__clang__) && SLEEFXXX__GNUC__ >= 8)
+&define SLEEFXXXSLEEF_FLOAT128_IS_IEEEQP
 &endif
 
-&if !defined(SLEEFXXXSLEEF_FLOAT128_IS_IEEEQPXXX) && defined(SLEEFXXX__SIZEOF_LONG_DOUBLE__XXX) && SLEEFXXX__SIZEOF_LONG_DOUBLE__XXX == 16 && (defined(SLEEFXXX__aarch64__XXX) || defined(SLEEFXXX__zarch__XXX))
-&define SLEEFXXXSLEEF_LONGDOUBLE_IS_IEEEQPXXX
+&if !defined(SLEEFXXXSLEEF_FLOAT128_IS_IEEEQP) && defined(SLEEFXXX__SIZEOF_LONG_DOUBLE__) && SLEEFXXX__SIZEOF_LONG_DOUBLE__ == 16 && (defined(SLEEFXXX__aarch64__) || defined(SLEEFXXX__zarch__))
+&define SLEEFXXXSLEEF_LONGDOUBLE_IS_IEEEQP
 &endif
 
-&if !defined(SLEEFXXXSleef_quad_DEFINEDXXX)
-&define SLEEFXXXSleef_quad_DEFINEDXXX
+&if !defined(SLEEFXXXSleef_quad_DEFINED)
+&define SLEEFXXXSleef_quad_DEFINED
 typedef struct { uint64_t x, y; } Sleef_uint64_2t;
-&if defined(SLEEFXXXSLEEF_FLOAT128_IS_IEEEQPXXX)
+&if defined(SLEEFXXXSLEEF_FLOAT128_IS_IEEEQP)
 typedef __float128 Sleef_quad;
-&define SLEEFXXXSLEEF_QUAD_CXXX(x) (x ## Q)
-&elif defined(SLEEFXXXSLEEF_LONGDOUBLE_IS_IEEEQPXXX)
+&define SLEEFXXXSLEEF_QUAD_C(x) (x ## Q)
+&elif defined(SLEEFXXXSLEEF_LONGDOUBLE_IS_IEEEQP)
 typedef long double Sleef_quad;
-&define SLEEFXXXSLEEF_QUAD_CXXX(x) (x ## L)
+&define SLEEFXXXSLEEF_QUAD_C(x) (x ## L)
 &else
 typedef Sleef_uint64_2t Sleef_quad;
 &endif
