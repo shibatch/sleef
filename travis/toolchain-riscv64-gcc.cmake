abd492ffac8c329f1217bd94fcd0cf63e0cb5adc
Ludovic Henry <git@ludovic.dev>
2023-11-14 18:26:42 +0000

    fixup! Use same compiler versions across all builds

--- travis/toolchain-riscv64-gcc.cmake
+++ travis/toolchain-riscv64-gcc.cmake
@@ -2,7 +2,7 @@ set(CMAKE_CROSSCOMPILING    TRUE)
 set(CMAKE_SYSTEM_NAME       "Linux")
 set(CMAKE_SYSTEM_PROCESSOR  "riscv64")
 
-find_program(CMAKE_C_COMPILER riscv64-unknown-linux-gnu-gcc)
+find_program(CMAKE_C_COMPILER NAMES riscv64-unknown-linux-gnu-gcc-12 riscv64-unknown-linux-gnu-gcc-11 riscv64-unknown-linux-gnu-gcc)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
