98378b4a94d0c66ec632d97c48ea40a735cd986d
Pierre Blanchard <pierre.blanchard@arm.com>
2024-01-03 14:52:48 +0000

    Remove Travis and Appveyor CI scripts.
    Keep toolchain files and rename them.

--- travis/before_script.arm64-gcc-sve.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ev
-mkdir sleef.build
-cd sleef.build
-export PATH=/opt/local/bin:$PATH
-export LD_LIBRARY_PATH=/opt/local/lib:$LD_LIBRARY_PATH
-export CC=gcc-10
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../install -DSLEEF_SHOW_CONFIG=1 -DEMULATOR=qemu-aarch64 -DENFORCE_TESTER3=TRUE -DBUILD_INLINE_HEADERS=TRUE -DBUILD_QUAD=TRUE -DBUILD_DFT=FALSE -DENFORCE_SVE=TRUE ..
