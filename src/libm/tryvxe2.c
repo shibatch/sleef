132d0bab1696fb70f59b8d99cd82eb0e9e0c643b
Joana Cruz <153619970+joanaxcruz@users.noreply.github.com>
2024-01-08 12:11:48 +0000

    Rename some global variables to avoid clashes (#499)
    This is a follow up from #454.

--- src/libm/tryvxe2.c
+++ src/libm/tryvxe2.c
@@ -1,8 +1,8 @@
 #include <vecintrin.h>
 
-__vector float sleef_cpuidtmp0;
-__vector int sleef_cpuidtmp1;
+__vector float sleef_cpuid_VXE2;
+__vector int sleef_cpuid_VXE1;
 
 void sleef_tryVXE2() {
-  sleef_cpuidtmp0 = vec_float(sleef_cpuidtmp1);
+  sleef_cpuid_VXE2 = vec_float(sleef_cpuid_VXE1);
 }
