794408d16c6d6449fe206abd06b4e9b6e2ecc33c
Pierre Blanchard <pierre.blanchard@arm.com>
2024-02-07 10:47:24 +0000

    Update copyright years.
    Add missing headers when needed.

--- src/arch/helperrvv.h
+++ src/arch/helperrvv.h
@@ -1,3 +1,8 @@
+//   Copyright Naoki Shibata and contributors 2023.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef HELPERRVV_H
 #define HELPERRVV_H
 
