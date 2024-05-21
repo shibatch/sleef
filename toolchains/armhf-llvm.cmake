fd618e6fbef1495987ba44738599ef76e85dde21
Ludovic Henry <git@ludovic.dev>
2024-05-21 12:54:13 +0000

    ci: test gcc versions 11 and 12 and llvm version 17 and 18

--- toolchains/armhf-llvm.cmake
+++ toolchains/armhf-llvm.cmake
@@ -4,7 +4,7 @@ SET (CMAKE_SYSTEM_PROCESSOR "armhf")
 
 SET(CMAKE_FIND_ROOT_PATH  /usr/arm-linux-gnueabihf /usr/include/arm-linux-gnueabihf /usr/lib/arm-linux-gnueabihf)
 
-find_program(CMAKE_C_COMPILER NAMES clang-17 clang-16 clang-15 clang-14 clang-13 clang)
+find_program(CMAKE_C_COMPILER NAMES clang-18 clang-17 clang-16 clang-15 clang-14 clang-13 clang)
 set(CMAKE_C_COMPILER_TARGET arm-linux-gnueabihf)
 
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
--- toolchains/native-llvm.cmake
+++ toolchains/native-llvm.cmake
@@ -1,1 +1,1 @@
-find_program(CMAKE_C_COMPILER NAMES clang-17 clang-16 clang-15 clang-14 clang-13 clang)
+find_program(CMAKE_C_COMPILER NAMES clang-18 clang-17 clang-16 clang-15 clang-14 clang-13 clang)
