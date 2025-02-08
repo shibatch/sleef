4da44f8d5604587c8270387185804d4ddcd5b6c0
Naoki Shibata <shibatch.sf.net@gmail.com>
2025-02-08 17:15:23 +0900

    This should add tester4

--- toolchains/ppc64el-gcc.cmake
+++ toolchains/ppc64el-gcc.cmake
@@ -4,18 +4,8 @@ SET (CMAKE_SYSTEM_PROCESSOR "ppc64")
 
 SET(CMAKE_FIND_ROOT_PATH  /usr/powerpc64le-linux-gnu /usr/include/powerpc64le-linux-gnu /usr/lib/powerpc64le-linux-gnu)
 
-find_program(CMAKE_C_COMPILER
-             NAMES powerpc64le-linux-gnu-gcc-14
-                   powerpc64le-linux-gnu-gcc-13
-                   powerpc64le-linux-gnu-gcc-12
-                   powerpc64le-linux-gnu-gcc-11
-                   powerpc64le-linux-gnu-gcc-10
-                   powerpc64le-linux-gnu-gcc-9
-                   powerpc64le-linux-gnu-gcc-8
-                   powerpc64le-linux-gnu-gcc-7
-                   powerpc64le-linux-gnu-gcc-6
-                   powerpc64le-linux-gnu-gcc-5
-                   ppc64el-cc)
+find_program(CMAKE_C_COMPILER NAMES powerpc64le-linux-gnu-gcc
+find_program(CMAKE_CXX_COMPILER NAMES powerpc64le-linux-gnu-g++)
 
 SET(CMAKE_AR /usr/powerpc64le-linux-gnu/bin/ar)
 
