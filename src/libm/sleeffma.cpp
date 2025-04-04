d192e7e2dbc4bf06c9c738b1b72729dfc41b42e3
Naoki Shibata <shibatch@users.sourceforge.net>
2025-04-05 00:37:59 +0900

    no message

--- src/libm/sleeffma.cpp
+++ src/libm/sleeffma.cpp
@@ -3,11 +3,13 @@
 using namespace tlfloat;
 
 extern "C" {
-  double Sleef_fma_internal(const double x, const double y, const double z) {
+#include "misc.h"
+
+  EXPORT double Sleef_fma_internal(const double x, const double y, const double z) {
     return (double)fma(Double(x), Double(y), Double(z));
   }
 
-  float Sleef_fmaf_internal(const float x, const float y, const float z) {
+  EXPORT float Sleef_fmaf_internal(const float x, const float y, const float z) {
     return (float)fma(Float(x), Float(y), Float(z));
   }
 }
